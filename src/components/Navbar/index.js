e056010ff97586f44e7526431913784426c929ef
ltrucco <56170216+ltrucco@users.noreply.github.com>
2021-05-12 23:45:14 -0300

    getBalance

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,47 +1,83 @@
-import React, {useState, useEffect} from 'react'
-import {FaBars} from 'react-icons/fa'
-import {IconContext} from 'react-icons/lib'
-import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu,NavLinks, NavItem, NavBtn, NavBtnLink} from './NavbarElements'
-import {animateScroll as scroll} from 'react-scroll'
-import {connectWallet} from '../../components/Metamask.js'
+import React, { useState, useEffect } from 'react'
+import { FaBars } from 'react-icons/fa'
+import { IconContext } from 'react-icons/lib'
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavLinks, NavItem, NavBtn, NavBtnLink } from './NavbarElements'
+import { animateScroll as scroll } from 'react-scroll'
+import { connectWallet } from '../../components/Metamask.js'
 import Web3 from 'web3'
+import {contract} from '../../constants/contract.js'
 
 
-const Navbar = ({toggle}) => {
+const Navbar = ( { toggle } ) => {
+    const web3 = new Web3('https://data-seed-prebsc-1-s1.binance.org:8545');
 
-    const [scrollNav, setScrollNav] = useState(false)
-    const [walletAddress, setWalletAddress] = useState('')
 
-    useEffect(() =>{
-        window.addEventListener('scroll', changeNav)
-    }, [])
+    const [scrollNav, setScrollNav] = useState( false )
+    const [walletAddress, setWalletAddress] = useState( '' )
 
-    React.useEffect(() => {
+    useEffect( () => {
+        window.addEventListener( 'scroll', changeNav )
+    }, [] )
+
+    React.useEffect( () => {
         const checkConnection = async () => {
- 
             // Check if browser is running Metamask
             let web3;
-            if (window.ethereum) {
-                web3 = new Web3(window.ethereum);
-            } else if (window.web3) {
-                web3 = new Web3(window.web3.currentProvider);
+            if ( window.ethereum ) {
+                web3 = new Web3( window.ethereum );
+            } else if ( window.web3 ) {
+                web3 = new Web3( window.web3.currentProvider );
             };
- 
+
             // Check if User is already connected by retrieving the accounts
             web3.eth.getAccounts()
-                .then(async (addr) => {
-                    if(addr[0])
-                        setWalletAddress(addr[0].substring(0,4) + '...' + addr[0].substring(addr[0].length -4 ,addr[0].length))
-                });
+                .then( async ( addr ) => {
+                    if ( addr[0] ) {
+                        setWalletAddress( addr[0].substring( 0, 4 ) + '...' + addr[0].substring( addr[0].length - 4, addr[0].length ) )
+
+                        let balance = await web3.eth.getBalance(addr[0])
+                        
+                        console.log(balance / 1000000000000000000)
+                        
+                        // const tokenAddress = {
+                        //     address: '0xc1eceb0821d492217bdbbaafaddd4ab1cb43da9d',
+                        //     token: 'MESSI'
+                        // }
+                        let tokenABI = [{
+                            "constant": true,
+                            "inputs": [
+                              {
+                                "name": "_owner",
+                                "type": "address"
+                              }
+                            ],
+                            "name": "balanceOf",
+                            "outputs": [
+                              {
+                                "name": "balance",
+                                "type": "uint256"
+                              }
+                            ],
+                            "payable": false,
+                            "type": "function"
+                          }]
+                        const tokenInst = new web3.eth.Contract(tokenABI,'0x2b591e99afe9f32eaa6214f7b7629768c40eeb39');
+                        const balanceMessi = await tokenInst.methods.balanceOf(addr[0]).call()
+                        console.log('messi' + balanceMessi)
+
+
+                    }
+                } );
+
         };
         checkConnection();
-    }, []);
+    }, [] );
 
     const changeNav = () => {
-        if(window.scrollY >= 200){
-            setScrollNav(true)
-        }else{
-            setScrollNav(false)
+        if ( window.scrollY >= 200 ) {
+            setScrollNav( true )
+        } else {
+            setScrollNav( false )
         }
     }
 
@@ -51,24 +87,30 @@ const Navbar = ({toggle}) => {
 
     const checkWallet = async () => {
         let walletInfo = await connectWallet()
-        if (walletInfo.hasMetamask){
+        if ( walletInfo.hasMetamask ) {
             let address = walletInfo.address[0]
-            setWalletAddress(address.substring(0,4) + '...' + address.substring(address.length -4 ,address.length))
-        }else{
-            alert(walletInfo.status)
+            setWalletAddress( address.substring( 0, 4 ) + '...' + address.substring( address.length - 4, address.length ) )
+            let balance = Web3.eth.getBalance( "0x407d73d8a49eeb85d32cf465507dd71d507100c1" )
+            console.log( balance )
+        } else {
+            alert( walletInfo.status )
         }
+    }
+
+    const disconectWallet = async () => {
+
     }
 
     return (
         <>
-            <IconContext.Provider value={{color: '#fff'}}>
+            <IconContext.Provider value={{ color: '#fff' }}>
                 <Nav scrollNav={scrollNav}>
                     <NavbarContainer>
                         <NavLogo to="/" onClick={toggleHome}>
                             Messi Finance
                         </NavLogo>
                         <MobileIcon onClick={toggle}>
-                            <FaBars/>
+                            <FaBars />
                         </MobileIcon>
                         <NavMenu>
                             <NavItem>
@@ -85,7 +127,7 @@ const Navbar = ({toggle}) => {
                             </NavItem>
                         </NavMenu>
                         <NavBtn>
-                            <NavBtnLink onClick={checkWallet}>{walletAddress === '' ? 'Connect' : walletAddress}</NavBtnLink>
+                            <NavBtnLink onClick={walletAddress === '' ? checkWallet : disconectWallet}>{walletAddress === '' ? 'Connect' : walletAddress}</NavBtnLink>
                         </NavBtn>
                     </NavbarContainer>
                 </Nav>
---
+++ src/constants/contract.js
@@ -0,0 +1,1 @@
+export const contract = '0xc1eceb0821d492217bdbbaafaddd4ab1cb43da9d';
