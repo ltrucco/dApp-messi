e40b7b2c622ba263f1bb58f5500d7f8065fe0c53
ltrucco <56170216+ltrucco@users.noreply.github.com>
2021-05-14 20:16:52 -0300

    Test youtube

--- src/components/AlertDialogVideo.js
+++ src/components/AlertDialogVideo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -6,9 +7,6 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Slide from '@material-ui/core/Slide';
 import YoutubeEmbed from './YoutubeEmbed';
-import ReactLivestream from 'react-livestream'
-import ReactTwitchEmbedVideo from "react-twitch-embed-video"
-
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
@@ -34,11 +32,10 @@ export default function AlertDialogVideo({closePopUp, }) {
         aria-describedby="alert-dialog-slide-description"
         fullWidth={true}
       >
-          {/* <div className='pointerEventNone' style={{position: 'absolute', width:'100%', height:'50%', background:'red', top:0, left: 0, pointerEvents: 'none', opacity: '10%', zIndex:'9999999999999'}}> */}
-          {/* </div> */}
-            {/* <YoutubeEmbed embedId="CSqX8TQvRVA" /> */}
-            <ReactTwitchEmbedVideo channel="PeinateCarlitos" />
-          
+          <div >
+            <YoutubeEmbed embedId="irkEy0980YE" />
+
+          </div>
       
       </Dialog>
   );
